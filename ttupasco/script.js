1de09ff6a606e784aee81df27fa0d8f1ccee314d
Nero-Gh <neroaboagye@gmail.com>
2021-01-10 21:46:15 +0000

    Updated

--- ttupasco/script.js
+++ ttupasco/script.js
@@ -2,7 +2,18 @@ var selectQuery = (element)=>document.querySelector(element);
 
 var togglebtn = selectQuery('#toggle');
 var menubars = selectQuery('#nav-list');
-
+var main_content = selectQuery('main');
+var main_header = selectQuery('header');
 togglebtn.addEventListener('click',()=>{
     menubars.classList.toggle('active');
+    main_content.classList.toggle('active');
+    header.classList.toggle('active');
 });
+
+window.onclick =(event)=>{
+    if(event.target.matches('.active')){
+        if(main_header.classList.contains('active')){
+            main_content.classList.remove('active');
+        }
+    }
+}
